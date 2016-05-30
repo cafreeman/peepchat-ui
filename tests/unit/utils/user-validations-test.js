3b8ae9a1aa2813a717e8447daed97938d301adb9
Chris Freeman <cfreeman.code@gmail.com>
2016-05-30 15:09:25 -0500

    fix typo is validations util test

--- tests/unit/utils/user-validations-test.js
+++ tests/unit/utils/user-validations-test.js
@@ -1,4 +1,4 @@
-import { email, password, passwordConfirmaton } from 'peepchat/utils/user-validations';
+import { email, password, passwordConfirmation } from 'peepchat/utils/user-validations';
 import { module, test } from 'qunit';
 
 module('Unit | Utility | user validations');
@@ -7,5 +7,5 @@ module('Unit | Utility | user validations');
 test('it works', function(assert) {
   assert.ok(email);
   assert.ok(password);
-  assert.ok(passwordConfirmaton);
+  assert.ok(passwordConfirmation);
 });
